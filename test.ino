1173ee91ac9baa5d4cfab9ed6718a32a942f3236
07602520 <raymon3838@gmail.com>
2017-05-22 19:37:24 +0800

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,25 @@
+#include <IRremote.h>
+ const int irReceiverPin = 2; 
+ IRrecv irrecv(irReceiverPin); 
+ decode_results results; 
+  
+ void setup()
+ {
+  Serial.begin(9600);
+//irrecv.blink13(true); 
+irrecv.enableIRIn(); 
+}
+ 
+void loop() {
+if (irrecv.decode(&results)) {
+//Serial.print("results value is "); 
+Serial.print("irCode: ");
+Serial.print(results.value, HEX);
+Serial.print(", bits: ");
+Serial.println(results.bits);
+//Serial.print(", decode_type is ");
+//Serial.println(results.decode_type);
+irrecv.resume(); 
+}
+}
+
